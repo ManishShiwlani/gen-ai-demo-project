eba1a6109072e8761b1aab5c2dd3852a4d5e8c99
manishshiwlani <manishshiwlani@gmail.com>
2024-02-08 11:31:32 -0500

    added Firestore

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAlfKmTdbGWtmBgRA-geDXp72JmxpNEkMo",
+    authDomain: "gen-ai-demo-8d4a2.firebaseapp.com",
+    projectId: "gen-ai-demo-8d4a2",
+    storageBucket: "gen-ai-demo-8d4a2.appspot.com",
+    messagingSenderId: "780920843524",
+    appId: "1:780920843524:web:9a96db5a12b01e7e727451"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
